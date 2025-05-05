bf8bf096eb36b25b2d6b63e6d4184c4a001a2864
Jack Teague <jackteague8@gmail.com>
2025-05-05 19:12:19 +0100

    newest

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ display_x, display_y = 1600, 1000
 cell_size = 50
 
 screen = pygame.display.set_mode((display_x, display_y))
-robot = robot.Robot(100, 100)
+robot = robot.Robot(50, display_y/2, width=25, sensor_count = 32)
 
 def new_course():
     course = []
@@ -21,59 +21,124 @@ def new_course():
 
         # Top portion of column
         course.append(obstacle.Obstacle(
-            (x+1)*150, 0, random.randint(10, 50), y_val))
+            (x+1)*150, 0, random.randint(40, 80), y_val))
         # Bottom portion of column
         course.append(obstacle.Obstacle(
             (x+1)*150, y_val+gap_size,
-            random.randint(10, 50), display_y-(y_val+gap_size)))
+            random.randint(40, 80), display_y-(y_val+gap_size)))
 
     return course
 
-exp_obs = new_course()  # Experimental obstacles: Those that never change
-ctrl_obs = new_course() # Control obstacles: Those that change every time
+# Experimental obstacles: Those that never change
+exp_obs = new_course()  
+course = exp_obs
+
+def raycast(agent, s_int = 10, max_dist = 600): # search_interval should be < min bar size.
+    global course
+    sensor_count = agent.sensor_count()
+    dists = [0 for _ in range(sensor_count)]
+    
+    pos = (agent.getx(), agent.gety())
+    rot = agent.rel_rot()
+    rps = 2*math.pi / sensor_count # Rotation Per Sensor
+
+    for i in range(sensor_count):
+        s_rot = i*rps +rot # Search rotation
+        dist = -s_int
+
+        searching = True
+        while searching:
+
+            # Expand search distance
+            dist += s_int 
+            search_pos = (dist*math.cos(s_rot) +pos[0], dist*math.sin(s_rot) +pos[1])
+            
+            for o in course:
+                if (o.collides(search_pos)
+                        and searching
+                        and dist <= max_dist
+                        or not (0 <= search_pos[0] <= display_x)
+                        or not (0 <= search_pos[1] <= display_y)):
+                    
+                    searching = False
+                    pygame.draw.rect(screen, "blue", search_pos+(10, 10))
+                    
+        dists[i] = dist/max_dist
+
+    return dists
+
 
 while running:
 
-    screen.fill("black")
+    states = []
+    actions = []
+    rewards = []
+    end_sim = False
 
-    time.sleep(1/60)
+    for i in range(5000):
+        if (not running) or end_sim: continue
 
-    if pygame.key.get_pressed()[pygame.K_q]: robot.l_accel(.01)
-    if pygame.key.get_pressed()[pygame.K_a]: robot.l_accel(-.01)
-    
-    if pygame.key.get_pressed()[pygame.K_e]: robot.r_accel(.01)
-    if pygame.key.get_pressed()[pygame.K_d]: robot.r_accel(-.01)
+        screen.fill("black")
 
-    collided = False
-    for a in robot.next_advances():
-        if collided: continue
-        for o in ctrl_obs:
-            if o.collides(a):
-                collided = True
+        state = raycast(robot)
+        state.append(robot.rel_rot())
+        states.append(state)
+        
+        action = robot.act(state)
 
-    if not collided:
-        robot.apply_advances()
-    else:
-        robot.collide()
-        robot.force_pos(100, 100)
-        ctrl_obs = new_course()
+        for m in range(len(action)):
+            action[m] += random.random() *.2
 
-    for o in ctrl_obs:
-        pygame.draw.rect(screen, o.colour, o.dims())
+        actions.append(action)
 
-    pygame.draw.line(screen, "white",
-                     robot.lwheel_pos(), robot.rwheel_pos(), 5)
-    
-    pygame.draw.rect(screen, "green",
-                     robot.lwheel_pos()+[5, 5])
-    pygame.draw.rect(screen, "red",
-                     robot.rwheel_pos()+[5, 5])
+        robot.set_lspeed(action[0] *.1)
+        robot.set_rspeed(action[1] *.1)
 
-    pygame.display.flip()
+        if pygame.key.get_pressed()[pygame.K_q]: robot.l_accel(.01)
+        if pygame.key.get_pressed()[pygame.K_a]: robot.l_accel(-.01)
+        
+        if pygame.key.get_pressed()[pygame.K_e]: robot.r_accel(.01)
+        if pygame.key.get_pressed()[pygame.K_d]: robot.r_accel(-.01)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        collided = False
+        for a in robot.next_advances(approximation = 5):
+            if collided: continue
+            for o in course:
+                if o.collides(a):
+                    collided = True
 
+        if (not collided
+                and 0 <= robot.getx() <= display_x
+                and 0 <= robot.gety() <= display_y):
+            robot.apply_advances()
+        else:
+            robot.collide()
+            end_sim = True
+
+        rewards.append((robot.getx() -50) /100)
+
+        for o in course:
+            pygame.draw.rect(screen, o.colour, o.dims())
+
+        pygame.draw.line(screen, "white",
+                         robot.lwheel_pos(), robot.rwheel_pos(), 5)
+        
+        pygame.draw.rect(screen, "green",
+                         robot.lwheel_pos()+[5, 5])
+        pygame.draw.rect(screen, "red",
+                         robot.rwheel_pos()+[5, 5])
+
+        raycast(robot)
+
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+    print(rewards[-1])
+    robot.train_step(states, actions, rewards)
+    robot.force_pos(50, display_y/2)
+    course = new_course()
 
 pygame.quit()
--- robot.py
+++ robot.py
@@ -1,47 +1,76 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
 import math
 
-class Wheel:
-
-    def __init__(self, x, y, drag, max_speed):
-        self.pos = [x, y]
-        self.speed = 0
-        self.__accel = False
-        
-        self.__max_speed = max_speed
-        self.__drag = drag
-
-    def accel(self, amount):
-        self.speed += amount
-        self.__accel = True
-        if self.speed > self.__max_speed: self.speed = self.__max_speed
-        if self.speed <-self.__max_speed: self.speed =-self.__max_speed
-
-    def apply_drag(self):
-        if not self.__accel:
-            self.speed /= self.__drag
-        self.__accel = False
-
-class Robot:
-
+class Robot(nn.Module):
     def __init__(self, x, y, sensor_count=16, width=50, drag=1.1, max_speed=.1):
+        super().__init__()
+        input_layer_size = sensor_count +1
+        self.model = nn.Sequential(
+            nn.Linear(input_layer_size, 2*input_layer_size),  # Input layer: 2x outputs as inputs
+            nn.ReLU(),
+            nn.Linear(2*input_layer_size, 2)    # Output layer: 2 outputs (lwheel rwheel)
+            )
+        self.optimizer = optim.Adam(self.parameters(), lr=.001)
+        
         w2 = width /2
         self.__lwheel = Wheel(x-w2, y, drag, max_speed)
         self.__rwheel = Wheel(x+w2, y, drag, max_speed)
         
-        self.__sensor_count = sensor_count
         self.__width = width
+        self.__sensor_count = sensor_count
+
+    # Called throughout the timeframe
+    def act(self, state):
+        state = torch.tensor(state, dtype=torch.float32)
+        return self.model(state)
+
+    # Used at the end of the timeframe to evaluate performance
+    def train_step(self, states, actions, rewards):
+        # Combine lists of tensors into single tensors
+        states = torch.tensor(states)
+        actions = torch.stack(actions)
+        # Turn reward list into tensor
+        rewards = torch.tensor(rewards)
+
+        # Predicted actions for each state IN ISOLATION.
+        p_actions = self.model(states)
+
+        # Loss measured to decline per-state error: Mean-squared error
+        loss = ((p_actions - actions) **2).mean(dim=1) * -rewards
+        loss = loss.mean()
+
+        # Pytorch magic
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+        
 
     def getx(self):
         return (self.__lwheel.pos[0] +self.__rwheel.pos[0])/2
+    def gety(self):
+        return (self.__lwheel.pos[1] +self.__rwheel.pos[1])/2
+    def sensor_count(self):
+        return self.__sensor_count
     
     def force_pos(self, x, y):
         w2 = self.__width /2
         self.__lwheel.pos = [x-w2, y]
         self.__rwheel.pos = [x+w2, y]
+        self.__lwheel.speed = 0
+        self.__rwheel.speed = 0
     
     def collide(self):
         self.__lwheel.speed = 0
         self.__rwheel.speed = 0
+
+    # Returns the rotation of the left wheel relative to the right one.
+    def rel_rot(self):
+        rot = math.atan2(self.__lwheel.pos[1]-self.__rwheel.pos[1],
+                         self.__lwheel.pos[0]-self.__rwheel.pos[0])
+        return rot / (2*math.pi)
     
     def next_advances(self, approximation=1):
         # Returns the positions, assuming no collision, of robot left/right/centre.
@@ -76,6 +105,11 @@ class Robot:
     def r_accel(self, amount):
         self.__rwheel.accel(amount)
 
+    def set_lspeed(self, amount):
+        self.__lwheel.set_speed(amount)
+    def set_rspeed(self, amount):
+        self.__rwheel.set_speed(amount)
+
     def lwheel_pos(self):
         return self.__lwheel.pos
     def rwheel_pos(self):
@@ -85,11 +119,11 @@ class Robot:
         self.__lwheel.apply_drag()
         self.__rwheel.apply_drag()
         
-        self.__l_advance(self.__lwheel.speed)
-        self.__r_advance(self.__rwheel.speed)
+        self.l_advance(self.__lwheel.speed)
+        self.r_advance(self.__rwheel.speed)
 
     # Movement per wheel
-    def __l_advance(self, amount):
+    def l_advance(self, amount):
         rot = math.atan2(self.__lwheel.pos[1]-self.__rwheel.pos[1],
                          self.__lwheel.pos[0]-self.__rwheel.pos[0])
         rot += amount
@@ -97,7 +131,7 @@ class Robot:
         self.__lwheel.pos[0] = self.__width * math.cos(rot) +self.__rwheel.pos[0]
         self.__lwheel.pos[1] = self.__width * math.sin(rot) +self.__rwheel.pos[1]
         
-    def __r_advance(self, amount):
+    def r_advance(self, amount):
         rot = math.atan2(self.__rwheel.pos[1]-self.__lwheel.pos[1],
                          self.__rwheel.pos[0]-self.__lwheel.pos[0])
         rot -= amount
@@ -106,7 +140,30 @@ class Robot:
         self.__rwheel.pos[1] = self.__width * math.sin(rot) +self.__lwheel.pos[1]
 
 
-
-
+# Inventing the wheel
+class Wheel:
+    def __init__(self, x, y, drag, max_speed):
+        self.pos = [x, y]
+        self.speed = 0
+        self.__accel = False
         
+        self.__max_speed = max_speed
+        self.__drag = drag
 
+    def set_speed(self, amount):
+        self.speed = amount
+        self.__movechecks()
+
+    def accel(self, amount):
+        self.speed += amount
+        self.__movechecks()
+
+    def __movechecks(self):
+        self.__accel = True
+        if self.speed > self.__max_speed: self.speed = self.__max_speed
+        if self.speed <-self.__max_speed: self.speed =-self.__max_speed
+
+    def apply_drag(self):
+        if not self.__accel:
+            self.speed /= self.__drag
+        self.__accel = False
